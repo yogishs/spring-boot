e825f54654504b977a09ae16db1a77af92924782
Yogish Shenoy <shenoy.yogish@gmail.com>
2016-12-31 22:14:49 +0530

    SQL insert statements for lookup tables

---
+++ microservices.sql
@@ -0,0 +1,19 @@
+INSERT INTO bpmdscm.occupation (id,"name") VALUES (
+1,'Pegawai Negeri');
+INSERT INTO bpmdscm.occupation (id,"name") VALUES (
+2,'Pegawai Swasta');
+INSERT INTO bpmdscm.occupation (id,"name") VALUES (
+3,'Buruh');
+INSERT INTO bpmdscm.occupation (id,"name") VALUES (
+4,'Pilot');
+INSERT INTO bpmdscm.occupation (id,"name") VALUES (
+5,'ABRI');
+
+INSERT INTO bpmdscm.country (id,"name") VALUES (
+1,'Indonesia');
+INSERT INTO bpmdscm.country (id,"name") VALUES (
+2,'Suria');
+INSERT INTO bpmdscm.country (id,"name") VALUES (
+3,'Turki');
+INSERT INTO bpmdscm.country (id,"name") VALUES (
+4,'USA');
